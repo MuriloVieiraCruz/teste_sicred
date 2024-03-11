d0052bd13e9236d72f95118263950b7d0b4b17c9
Murilo Vieira <murilo12super@gmail.com>
2024-03-11 09:11:04 -0300

    first commit

---
+++ src/main/java/com/murilovieira/testesicred/TestesicredApplication.java
@@ -0,0 +1,13 @@
+package com.murilovieira.testesicred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesicredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesicredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilovieira/testesicred/TestesicredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilovieira.testesicred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesicredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
